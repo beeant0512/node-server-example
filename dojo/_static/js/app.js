cb2430d79f80d0a33c4b46c77b33299eeb9e135c
Beeant <huangxb0512@gmail.com>
2016-11-27 15:51:58 +0800

    menu click to show tree in left sider(has bugs)

--- dojo/_static/js/app.js
+++ dojo/_static/js/app.js
@@ -6,6 +6,7 @@ require(["dojo/parser", "dijit/layout/BorderContainer", "dijit/layout/TabContain
   "dijit/layout/ContentPane"]);
 
 require([
+  "dojo/dom-construct",
   "dijit/MenuBar",
   "dijit/MenuBarItem",
   "dojo/request",
@@ -16,39 +17,44 @@ require([
   "dijit/registry",
   "dojox/layout/ContentPane",
   "dojo/ready"
-], function (MenuBar, MenuBarItem, request, array, Memory, ObjectStoreModel, Tree, registry, ContentPane, ready) {
+], function (domConstruct, MenuBar, MenuBarItem, request, array, Memory, ObjectStoreModel, Tree, registry, ContentPane, ready) {
   ready(function () {
     var topMenu = new MenuBar({"region": "top"});
     var tabs = registry.byId("tabContainer");
     request.post("/app/menu").then(
       function (appMenu) {
         var appMenu = JSON.parse(appMenu);
-        array.forEach(appMenu, function (item, index) {
+        // set up the store to get the tree data
+        var menuStore = new Memory({
+          data: [appMenu],
+          getChildren: function (object) {
+            return object.children || [];
+          }
+        });
+        // set up the model, assigning menuStore, and assigning method to identify leaf nodes of tree
+        var menuModel = new ObjectStoreModel({
+          store: menuStore,
+          mayHaveChildren: function (item) {
+            return "children" in item;
+          }
+        });
+        array.forEach(appMenu, function (item) {
           topMenu.addChild(new MenuBarItem({
             label: item.name,
             id: item.id,
-            onClick: function (e) {
-              console.log(e);
-              console.log(this);
-              // set up the store to get the tree data
-              var menuStore = new Memory({
-                data: [item],
-                getChildren: function (object) {
-                  return object.children || [];
-                }
-              });
-
-              // set up the model, assigning menuStore, and assigning method to identify leaf nodes of tree
-              var menuModel = new ObjectStoreModel({
-                store: menuStore,
-                query: {id: item.id},
-                mayHaveChildren: function (item) {
-                  return "children" in item;
-                }
-              });
-
+            onClick: function () {
+              menuStore.setData([item]);
+              console.log(item);
+              menuModel.query = {id: item.id};
+              menuModel.store = menuStore;
               // set up the tree, assigning menuModel;
-              var menuTree = new Tree({
+              var sideMenuId = 'leftSideMenu' + item.id;
+              var menuTree = registry.byId(sideMenuId);
+              domConstruct.create('div',{"id":sideMenuId},"leftSide");
+              if (menuTree) {
+                menuTree.destroy();
+              }
+              menuTree = new Tree({
                 model: menuModel,
                 onOpenClick: true,
                 onClick: function (item) {
@@ -68,7 +74,7 @@ require([
                     tabs.selectChild(pane);
                   }
                 }
-              }, "leftSideMenu");
+              }, sideMenuId);
               menuTree.startup();
             }
           }))
